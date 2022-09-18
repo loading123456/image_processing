2bfb7277378cc7120a1201406fe4202445533d9c
ging-dev <thanh1101dev@gmail.com>
2022-09-18 22:16:03 +0700

    refactor

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# image_processing
+# Image Processing
+
+## Installation
+
+```bash
+sudo apt-get install tesseract-ocr
+pip install -r requirements.txt
+```
+
+## Testing
+```bash
+python3 main.py input/2.png
+```
--- main.py
+++ main.py
@@ -1,69 +1,37 @@
+from os import path
+from sys import argv
 import pytesseract
-from pytesseract import Output
 import cv2
-import socket
 
-input = []
-isBusy = False
+def image_processing(input: str):
+    print("Scanning: ", input)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-serverAddress = ('localhost', 2021)
+    if not path.isfile(input):
+        print("File doesn't exist.")
 
+        return;
 
-def listening():
-    global input
-    
-    while True:
-        data = sock.recvfrom(4096)
-        if data:
-            input.append(data.decode()) 
-            if not isBusy:
-                imageProcessing()
+    filename = path.basename(input)
+    data = {}
+    img = cv2.imread(input)
 
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
+    rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
 
+    dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1)
+    contours, _ = cv2.findContours(dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
+    im2 = img.copy()
 
-def imageProcessing():
-    global input
-    global isBusy
+    for cnt in contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+        cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cropped = im2[y:y + h, x:x + w]
+        text = pytesseract.image_to_string(cropped)
+        data[''.join(map(str, [x, y, x+w, y+h]))] = text
 
-    isBusy = True
+    cv2.imwrite('output/' + filename, im2)
+    print('Done!');
 
-    print("Scanning: ",input[0])
-
-    if len(input) > 0:
-        data = {}
-        link = input[0]
-        img = cv2.imread('input/' + link)
-        
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        ret, thresh1 = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
-        rect_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 18))
-        
-        dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1)
-        contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,
-                                                        cv2.CHAIN_APPROX_NONE)
-        im2 = img.copy()
-        
-        for cnt in contours:
-            x, y, w, h = cv2.boundingRect(cnt)
-            rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            cropped = im2[y:y + h, x:x + w]
-            text = pytesseract.image_to_string(cropped)
-            data[''.join(map(str, [x, y, x+w, y+h]))] = text
-        
-        cv2.imwrite('output/' + link, im2)
-        sock.sendto(str(data).encode(), serverAddress)
-
-    input.pop(0)
-
-    if len(input) > 0:
-        imageProcessing()
-    else:
-        isBusy = False
-
-    print("     -->Finished: ", link)
-
-
-
-listening()
+image_processing(argv[1]);
